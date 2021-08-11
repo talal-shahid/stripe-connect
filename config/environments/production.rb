953f7688adf9fc4ed3ac357125ccecd822cc2842
Usman Ghani <usman.ghani720@gmail.com>
2021-08-11 16:04:14 +0500

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,22 +93,12 @@ Rails.application.configure do
   
   config.action_mailer.perform_deliveries = true
   
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://young-mountain-95854.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV["EMAIL"],
-    password:             ENV["PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
-
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.com',
+    address:              'smtp.sendgrid.net',
     port:                 '587',
     domain:               'young-mountain-95854.herokuapp.com',
     user_name:            'apikey',
