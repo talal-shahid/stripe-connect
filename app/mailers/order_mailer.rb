6a626d5714d30f84c243d5c7452242144dee0d0d
Usman Ghani <usman.ghani720@gmail.com>
2021-08-11 16:07:14 +0500

    fixes

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,7 @@
 class OrderMailer < ApplicationMailer
-    default :from => 'usman.ghani720@gmail.com'
+    default :from => 'connect@theconnectioninstitute.net'
     def send_invite(email)
         @email = email
-        mail(to: email, subject: "You have received an invitation", from: 'usman.ghani720@gmail.com')
+        mail(to: email, subject: "You have received an invitation", from: 'connect@theconnectioninstitute.net')
     end
 end
